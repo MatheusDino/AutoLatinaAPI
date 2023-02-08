ae040e97bbe56728ac6655f48fd6c370c2e1982c
MatheusDino <66699413+MatheusDino@users.noreply.github.com>
2023-02-08 00:04:39 -0300

    Add files via upload

--- controllers/ProprietariosController.java
+++ controllers/ProprietariosController.java
@@ -1,10 +1,10 @@
 package com.autolatina.controllers;
 
 import java.util.List;
-
-
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,19 +38,30 @@ public class ProprietariosController {
 	}
 	
 	@PostMapping("/dono")
-	public Proprietarios cadastrarProps(@RequestBody Proprietarios props) {
+	public String cadastrarProps(@RequestBody Proprietarios props) {
 		Proprietarios prop = props;
-		
-		return propRepo.save(prop);
+		try {
+			propRepo.save(prop);
+			return "Proprietario cadastrado com sucesso. ";			
+		}
+		catch(DataIntegrityViolationException e) {
+			return "Por favor preencha todas as informações e tente novamente. ";
+		}
 	}
 	
 	@PutMapping("/dono/{iden}")
-	public Proprietarios editarProps(@PathVariable("iden") long iden, @RequestBody Proprietarios props) {
-		Proprietarios propData = propRepo.findById(iden).get();
-		Proprietarios proprietario = propData;
-		proprietario.setNome(props.getNome());
-		proprietario.setEmail(props.getEmail());
-		return propRepo.save(proprietario);
+	public String editarProps(@PathVariable("iden") long iden, @RequestBody Proprietarios props) {
+		try {
+			Proprietarios propData = propRepo.findById(iden).get();
+			Proprietarios proprietario = propData;
+			proprietario.setNome(props.getNome());
+			proprietario.setEmail(props.getEmail());
+			propRepo.save(proprietario);	
+			return "Proprietário alterado com sucesso. ";
+		}
+		catch(DataIntegrityViolationException e) {
+			return "Por favor preencha todas as informações e tente novamente. ";
+		}
 	}
 	
 	@DeleteMapping("/dono")
@@ -79,22 +90,46 @@ public class ProprietariosController {
 	}
 	
 	@PostMapping("/veic")
-	public Veiculos cadastrarVeic(@RequestBody Veiculos veics) {
-		Veiculos veic = veics;
-		return veicRepo.save(veic);
+	public String cadastrarVeic(@RequestBody Veiculos veics) {
+		Veiculos veiculo = veics;
+		try {
+			if(veiculo.getAno() == 0) {
+				throw new DataIntegrityViolationException("Falta o ano do veículo.");
+			}
+			veicRepo.save(veiculo);
+			return "Veículo salvo com sucesso";
+		}
+		catch(DataIntegrityViolationException e) {
+			return "Por favor preencha todas as informações e tente novamente. ";
+		}
 	}
 	
-	@PutMapping("/veic/{chassis}/{iden}")
-	public Veiculos editarVeic(@PathVariable("chassis") long chassis, @PathVariable("iden") long iden, @RequestBody Veiculos veics) {
-		Proprietarios propData = propRepo.findById(iden).get();
-		Veiculos veicData = veicRepo.findById(chassis).get();
-		Veiculos veiculo = veicData;
-		veiculo.setMarca(veics.getMarca());
-		veiculo.setModelo(veics.getModelo());
-		veiculo.setMotor(veics.getMotor());
-		veiculo.setAno(veics.getAno());
-		veiculo.setPropIden(propData.getIdent());
-		return veicRepo.save(veiculo);
+	@PutMapping("/veic/{chassis}")
+	public String editarVeic(@PathVariable("chassis") long chassis, @RequestBody Veiculos veics) {
+		try {
+			if(veics.getAno() == 0) {
+				throw new DataIntegrityViolationException("Falta o ano do veículo");
+			}
+			Veiculos veicData = veicRepo.findById(chassis).get();
+			Veiculos veiculo = veicData;
+			veiculo.setMarca(veics.getMarca());
+			veiculo.setModelo(veics.getModelo());
+			veiculo.setMotor(veics.getMotor());
+			veiculo.setAno(veics.getAno());
+			
+			if(veics.getPropIden() != 0) {
+				Proprietarios propData = propRepo.findById(veics.getPropIden()).get();
+				veiculo.setPropIden(propData.getIdent());				
+			}
+			veicRepo.save(veiculo);
+			return "Veiculo alterado com sucesso.";
+		}
+		catch(DataIntegrityViolationException nullInfo) {
+			return "Por favor preencha todas as informações e tente novamente. Erro: " + nullInfo.getMessage();
+		}
+		catch(NoSuchElementException NSEE) {
+			return "Por favor preencha todas as informações e tente novamente. Erro: " + NSEE.getMessage();
+		}
 	}
 	
 	@DeleteMapping("/veic")
